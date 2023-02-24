1b082e56fc158b831a077430f5753c2e86c4661c
albin.guignabert <aguignabert.externe@bedrockstreaming.com>
2023-02-24 15:11:27 +0100

    feat: add readme, split client to separate file, improve output

---
+++ readme.md
@@ -0,0 +1,28 @@
+# Kinesis-tools-rust
+
+This project allow to read to an AWS Kinesis stream
+
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/Keenegan/kinesis-tools-rust
+```
+
+Go to the project directory
+
+```bash
+  cd kinesis-tools-rust
+```
+
+Read stream
+
+```bash
+  cargo run -- --stream <your_stream>
+```
+
+Print help
+```bash
+ cargo run -- --help
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
 #![allow(dead_code, unused_variables, unused)]
 
-use aws_sdk_kinesis::{Client, Error, Region};
-use std::{env};
+use std::env;
+use std::io::prelude::*;
 use std::str;
 use std::sync::Arc;
+
 use aws_config::profile::ProfileFileCredentialsProvider;
 use aws_config::sts::AssumeRoleProvider;
+use aws_sdk_kinesis::{Client, Error, Region};
 use aws_sdk_kinesis::model::{Shard, ShardIteratorType};
-use std::io::prelude::*;
+use clap::{arg, Parser};
+use flate2::read::ZlibDecoder;
 use tokio::sync::mpsc;
 use tokio::task;
-use flate2::read::{ZlibDecoder};
-use clap::{arg, Parser};
+
+use crate::client::{ClientConfig, get_client};
+
+mod client;
 
 #[derive(Parser, Clone)]
-struct Args {
+pub struct Args {
     #[arg(long)]
     stream: String,
     #[arg(long)]
@@ -30,54 +35,42 @@ struct Args {
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let args = Args::parse();
-    let client = get_client(args.clone()).await;
-    read_stream(&client, args.clone()).await;
-    Ok(())
-}
-
-async fn show_streams(client: &Client) -> Result<(), Error> {
-    let resp = client.list_streams().send().await?;
-
-    println!("Stream names:");
-
-    let streams = resp.stream_names.unwrap_or_default();
-    for stream in &streams {
-        println!("  {}", stream);
-    }
-
-    println!("Found {} stream(s)", streams.len());
-
-    Ok(())
-}
-
-async fn show_stream(client: &Client, stream: &String) -> Result<(), Error> {
-    let resp = client.describe_stream().stream_name(stream).send().await?;
-    let desc = resp.stream_description.unwrap();
-    let shards = desc.shards.unwrap();
-
-    println!("Stream description:");
-    println!("  Name:              {}:", desc.stream_name.unwrap());
-    println!("  Status:            {:?}", desc.stream_status.unwrap());
-    println!("  Open shards:       {:?}", shards.len());
-    shards.iter()
-        .map(|shard| println!("    {}", shard.shard_id().unwrap()))
-        .collect::<Vec<_>>();
-    println!("  Retention (hours): {}", desc.retention_period_hours.unwrap());
-    println!("  Encryption:        {:?}", desc.encryption_type.unwrap());
-    println!("  ARN:               {:?}", desc.stream_arn.unwrap());
-    Ok(())
-}
-
-async fn read_stream(client: &Client, args: Args) -> Result<(), Error> {
     let stream = &args.stream;
-    show_stream(client, stream).await;
-    let resp = client.describe_stream().stream_name(stream).send().await?;
+    let (client, client_config) = get_client(args.clone()).await;
+    read_stream(&client, &client_config, stream).await;
+    Ok(())
+}
+
+async fn read_stream(client: &Client, client_config: &ClientConfig, stream: &String) -> Result<(), Error> {
+    let resp = client.describe_stream().stream_name(stream).send().await.expect("No stream found.");
     let desc = resp.stream_description.unwrap();
     let shards = desc.shards.unwrap();
     let shard_count = shards.len();
+
+    println!("========================================================================================");
+    println!("|                                    Context loaded                                    |");
+    println!("========================================================================================");
+    println!("AWS_REGION                     | {}", client_config.region);
+    println!("AWS_PROFILE                    | {}", client_config.profile);
+    println!("AWS_PROFILE                    | {}", client_config.role_arn);
+    println!("AWS_PROFILE                    | {}", client_config.session_name);
+
+    println!("========================================================================================");
+    println!("|                                    Stream description                                |");
+    println!("========================================================================================");
+    println!("Name:              {}:", desc.stream_name.unwrap());
+    println!("ARN:               {}:", desc.stream_arn.unwrap());
+    println!("Status:            {:?}", desc.stream_status.unwrap());
+    println!("Open shards:       {:?}", shards.len());
+    shards.iter()
+        .map(|shard| println!("    {}", shard.shard_id().unwrap()))
+        .collect::<Vec<_>>();
+    println!("  Encryption:        {:?}", desc.encryption_type.unwrap());
     let (tx, mut rx) = mpsc::channel(shards.len());
 
-    println!("Listening kinesis events from {} shards", shard_count);
+    println!("========================================================================================");
+    println!("|                 Listening kinesis events from {shard_count} shards                               |");
+    println!("========================================================================================");
     for shard in shards {
         let client_clone = client.clone();
         let tx_clone = tx.clone();
@@ -118,28 +111,7 @@ async fn listen_to_shard(shard: Shard, client: Client, stream: String) {
             let mut decoder = ZlibDecoder::new(data);
             let mut result = String::new();
             decoder.read_to_string(&mut result).unwrap();
-            println!("{}", result);
+            println!("{result}");
         }
     }
-}
-
-async fn get_client(args: Args) -> Client {
-    dotenv::dotenv().is_ok();
-    let region = args.aws_region.unwrap_or(env::var("AWS_REGION").unwrap_or(String::from("eu-west-3")));
-    let profile = args.aws_profile.unwrap_or(env::var("AWS_PROFILE_NAME").unwrap());
-    let role_arn = args.aws_role_arn.unwrap_or(env::var("AWS_ROLE_ARN").unwrap());
-    let session_name = args.aws_session_name.unwrap_or(env::var("AWS_SESSION_NAME").unwrap());
-
-    let credentials_provider = ProfileFileCredentialsProvider::builder()
-        .profile_name(profile)
-        .build();
-    let provider = AssumeRoleProvider::builder(role_arn)
-        .region(Region::new(region))
-        .session_name(session_name)
-        .build(Arc::new(credentials_provider) as Arc<_>);
-    let shared_config = aws_config::from_env()
-        .credentials_provider(provider)
-        .load()
-        .await;
-    Client::new(&shared_config)
 }
