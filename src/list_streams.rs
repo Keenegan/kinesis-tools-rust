207fa90a9d47ed31dda5a7b28df279da5d0578ac
albin.guignabert <aguignabert.externe@bedrockstreaming.com>
2023-03-09 14:09:24 +0100

    feat: wip better error handling

--- src/list_streams.rs
+++ src/list_streams.rs
@@ -3,8 +3,16 @@ use std::sync::Arc;
 use aws_sdk_kinesis::{Client, Error};
 
 pub async fn list_streams(client: Arc<Client>) -> Result<(), Error> {
-    let resp = client.list_streams().send().await.expect("No stream found");
-    let streams = resp.stream_names.unwrap();
+    let list_stream_output = match client.list_streams().send().await {
+        Ok(list_stream_output) => list_stream_output,
+        Err(error) => panic!("Problem listing the streams : {}", error)
+    };
+
+    let streams  = list_stream_output.stream_names.unwrap();
+    // Fix partial move here
+    if list_stream_output.has_more_streams().unwrap() {
+        dbg!("more");
+    }
 
     println!("========================================================================================");
     println!("|                                   {} Streams found                                   |", streams.len());
