8d9229956534748db9bd23f49403bd9799de562e
albin.guignabert <aguignabert.externe@bedrockstreaming.com>
2023-05-31 14:05:17 +0200

    fix: ci fix

--- src/list_streams.rs
+++ src/list_streams.rs
@@ -8,16 +8,29 @@ pub async fn list_streams(client: Arc<Client>) -> Result<(), Error> {
         .send()
         .await
         .expect("Error listing the streams");
-    let streams = list_stream_output.stream_names().unwrap();
 
     println!("------------------------------------------------------------");
-    println!("Listing AWS Kinesis Data Streams in {}", client.conf().region().unwrap());
+    println!(
+        "Listing AWS Kinesis Data Streams in {}",
+        client.conf().region().unwrap()
+    );
     println!("------------------------------------------------------------");
     for stream_summary in list_stream_output.stream_summaries().unwrap() {
         println!("- Name: {}", stream_summary.stream_name().unwrap());
         println!("- ARN: {}", stream_summary.stream_arn().unwrap());
-        println!("- Status: {}", stream_summary.stream_status().unwrap().as_str());
-        println!("- Mode: {}", stream_summary.stream_mode_details().unwrap().stream_mode().unwrap().as_str());
+        println!(
+            "- Status: {}",
+            stream_summary.stream_status().unwrap().as_str()
+        );
+        println!(
+            "- Mode: {}",
+            stream_summary
+                .stream_mode_details()
+                .unwrap()
+                .stream_mode()
+                .unwrap()
+                .as_str()
+        );
         println!("------------------------------------------------------------");
     }
 
